32d3cc62ae44ee03f479c382f149fbf800c5b3a1
Oscar Bautista <oscar04262000@gmail.com>
2023-01-03 14:10:53 -0600

    seccion 2 terminada

--- 02.-Aplicacion/my_app/lib/main.dart
+++ 02.-Aplicacion/my_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'package:my_app/screens/home_screen.dart';
+// import 'package:my_app/screens/home_screen.dart';
+import 'package:my_app/screens/counter_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen()
+      home: CounterScreen()
     );
   }
 
--- 02.-Aplicacion/my_app/lib/screens/home_screen.dart
+++ 02.-Aplicacion/my_app/lib/screens/home_screen.dart
@@ -1,20 +1,42 @@
 import 'package:flutter/material.dart';
 
+// StatelessWidget: dibuja los widget en la pantalla, pero son de manera estaticos, es decir no cambian despues de su ejecución
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  
   // Scaffold: Lienzo la cual ya tiene opciones par cargar animaciones
   // BuildContext: es el arbol de widgets
   @override
   Widget build(BuildContext context) {
+
+    const fontSize30 =  TextStyle(fontSize: 30);
+    int contador = 0;
+
     return  Scaffold(
       appBar: AppBar(
         title: const Text('HomeScreen'),
         elevation: 10.0,
       ),
-      body: const Center(
-        child: Text('Hola desde Home Screen'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          
+          children: <Widget> [
+            const Text('Número de clicks', style: fontSize30),
+            Text('$contador', style: fontSize30,)
+          ],
+        ),
       ),
+      floatingActionButtonLocation:  FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.plus_one),
+        onPressed: () {
+          // A qui crear funcion
+          contador++;
+          print(contador);
+        }
+      ) ,
     );
   }
 
